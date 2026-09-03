649c3ff155f2374e81a09c1db87e9ad0021fcef9
JimmyVu <huyquoc.vq@gmail.com>
2026-09-03 15:27:54 +0700

    Align iOS RevenueCat entitlement with Honoured backend

--- ios/Honoured/AppConfig.swift
+++ ios/Honoured/AppConfig.swift
@@ -3,7 +3,7 @@ import Foundation
 enum AppConfig {
     static let webAppURL = URL(string: "https://honour-your-word.lovable.app")!
     static let bridgeVersion = 1
-    static let revenueCatEntitlementID = "pro"
+    static let revenueCatEntitlementID = "honoured_plus"
 
     static var revenueCatAPIKey: String {
         let raw = Bundle.main.object(forInfoDictionaryKey: "RevenueCatAPIKey") as? String ?? ""
