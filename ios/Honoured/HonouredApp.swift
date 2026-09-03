4d93af4b6f2c66b23048570052f748e130dd0d63
JimmyVu <huyquoc.vq@gmail.com>
2026-09-03 15:15:23 +0700

    Configure RevenueCat on iOS app launch

--- ios/Honoured/HonouredApp.swift
+++ ios/Honoured/HonouredApp.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @main
 struct HonouredApp: App {
+    init() {
+        SubscriptionService.shared.configureIfPossible()
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
