8794c214651fa3c3136fcb99f46c4f889f333cb7
JimmyVu <huyquoc.vq@gmail.com>
2026-09-03 15:16:13 +0700

    Add RevenueCat entitlement config on Android

--- android/app/src/main/java/com/honoured/app/AppConfig.kt
+++ android/app/src/main/java/com/honoured/app/AppConfig.kt
@@ -4,4 +4,5 @@ object AppConfig {
     const val WEB_APP_URL = "https://honour-your-word.lovable.app"
     const val WEB_APP_HOST = "honour-your-word.lovable.app"
     const val BRIDGE_VERSION = 1
+    const val REVENUECAT_ENTITLEMENT_ID = "pro"
 }
